271050576300b8fdd57c1efb62d632b6633e6387
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-09-08 17:37:01 +0100

    Add test for running redis

--- test/integration/default/redis.rb
+++ test/integration/default/redis.rb
@@ -1,3 +1,11 @@
 describe package('redis') do
   it { should be_installed }
 end
+
+describe service('redis') do
+  it { should be_running }
+end
+
+describe port(6379) do
+  it { should be_listening }
+end
