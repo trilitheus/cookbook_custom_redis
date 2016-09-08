d8e4a9f3a76ba132caa8e216516c1c44e1ec4946
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-09-08 17:37:25 +0100

    Add monit check for redis

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,5 +2,10 @@ include_recipe 'yum-epel::default'
 include_recipe 'redisio::default'
 include_recipe 'monit-ng::default'
 
-#monit_check 'redis' do
-#  check_id
+monit_check 'redis' do
+  check_type 'process'
+  id_type 'matching'
+  check_id 'redis-server'
+  start '/bin/systemctl start redis'
+  stop '/bin/systemctl stop redis'
+end
