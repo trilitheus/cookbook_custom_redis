d1a0d9bb24b400d1834ac70d800cb54818ff0879
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-09-09 13:35:52 +0100

    Log to local fs directly

--- attributes/redis.rb
+++ attributes/redis.rb
@@ -1,2 +1,5 @@
 default['redisio']['package_install'] = true
 default['redisio']['version'] = '2.8.19-2.el7'
+default['redisio']['default_settings']['address'] = '0.0.0.0'
+default['redisio']['default_settings']['syslogenabled'] = 'no'
+default['redisio']['default_settings']['logfile'] = '/var/log/redis/redis.log'
