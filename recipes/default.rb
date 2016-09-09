09c9e72a7d9b862cb37c245737540acba088447d
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-09-09 13:35:32 +0100

    extra kernel params plus link in conf file so systemd can find it

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,8 +7,22 @@ sysctl_param 'vm.overcommit_memory' do
   value 1
 end
 
-sysctl_param 'net.core.somaxconn' do
-  value 511
+%w(
+net.core.somaxconn
+net.ipv4.tcp_max_syn_backlog 
+).each do |kern|
+  sysctl_param kern do
+    value 511
+  end
+end
+
+file '/etc/redis.conf' do
+  action :delete
+  not_if { ::File.symlink?('/etc/redis.conf') }
+end
+
+link '/etc/redis.conf' do
+  to '/etc/redis/6379.conf'
 end
 
 monit_check 'redis' do
