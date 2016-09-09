e1b19d637fb74fa734e1f7016b2e1bf3c2b68056
Roy Crombleholme <roy.crombleholme@pam-it.com>
2016-09-09 14:06:50 +0100

    Set virtualization type in chefspec tests
    This allows us to add some logic in attributes

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -13,6 +13,7 @@ describe 'custom_redis::default' do
         node.automatic['memory']['total'] = 4096
         node.automatic['os'] = 'linux'
         node.automatic['platform_family'] = 'rhel'
+        node.automatic['virtualization']['system'] = 'vmware'
       end.converge(described_recipe)
     end
 
@@ -26,6 +27,22 @@ describe 'custom_redis::default' do
 
     it 'includes the monit-ng cookbook' do
       expect(chef_run).to include_recipe('monit-ng::default')
+    end
+
+    it 'includes the sysctl cookbook' do
+      expect(chef_run).to include_recipe('sysctl::default')
+    end
+
+    it 'sets net.core.somaxconn to 511' do
+      expect(chef_run).to apply_sysctl_param('net.core.somaxconn').with_value('511')
+    end
+
+    it 'sets net.ipv4.tcp_max_syn_backlog to 511' do
+      expect(chef_run).to apply_sysctl_param('net.ipv4.tcp_max_syn_backlog').with_value('511')
+    end
+
+    it 'sets vm.overcommit_memory to 1' do
+      expect(chef_run).to apply_sysctl_param('vm.overcommit_memory').with_value('1')
     end
   end
 end
